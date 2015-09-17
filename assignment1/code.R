bc6a666c4c029377bb76bef7714445393c9f18c0
Sebastiaan Jong <sebastiaan.jong@gmail.com>
2015-09-17 18:28:16 +0200

    added function to split shit in two

--- assignment1/code.R
+++ assignment1/code.R
@@ -1,38 +1,79 @@
-credit.dat <- read.csv('~/UU/MDM/credit.txt')
-y <- c(1,0,1,1,1,0,0,1,1,0,1)
-
+# Gini index, two class case.
 impurity <- function(vect){
   freq <- (sum(vect == 0))/length(vect)
   return (freq * (1-freq))
 }
 
-split <- function(vect, pivot){
-	
-}
-
-optimalsplit <- function(x,y){
-
-  sorted <- sort(unique(x))
-  splitpoints <- (sorted[1:(length(x)-1)]+sorted[2:length(x)])/2
+# Given a vector of integers, returns a list of potential splitpoints.
+splitpoints <- function(vect){
+  sorted <- sort(unique(vect))
+  splitpoints <- (sorted[1:(length(vect)-1)]+sorted[2:length(vect)])/2
   splitpoints <- splitpoints[!is.na(splitpoints)]
-#  imp <- impurity(y)
   
-  ind <- order(x);
-  dat <- (cbind(x[ind], y[ind]))
+  return (splitpoints)
 }
+
+# x - input dataframe
+# y - classes vector
+# c - column index
+optimalsplit <- function(x, y, c){
+  potentialsplits <- splitpoints(x[,c])
   
-#   bestsplit <- c(0,Inf)
-#   for(i in 1:length(splitpoints)){
-#     
-#     temp <- impurity(y[])
-#     if(temp < bestsplit[2])
-#     {
-#       bestsplit <- c(splitpoints[i], temp);
-#     }
-#   }
-#   
-#   return (bestsplit[1]);
+  splits <- mapply(function(pivot) split(x, y, c, pivot), potentialsplits)
+  
+  print(splits)
+
+  return(potentialsplits)
+}
+
+# split the dataset into two parts and return the impurity for this split
+split <- function(x, y, c, pivot){
+  ordering <- order(x[,c])
+  x.ordered <- x[ordering,]
+  y.ordered <- y[ordering]
+  
+  x.left <- subset(x.ordered, x.ordered[,c] < pivot)
+  x.right <- subset(x.ordered, x.ordered[,c] > pivot)
+  
+  offset <- nrow(x.left)
+  
+  y.left <- y.ordered[1:offset]
+  y.right <- y.ordered[offset+1:(length(y)-offset)]
+  y.length <- length(y)
+  
+  i.left = impurity(y.left)*(length(y.left)/y.length)
+  i.right = impurity(y.right)*(length(y.right)/y.length)
+  score = i.left + i.right
+  
+  return(list(score, x.left, x.right))
+}
+
+# x - input data matrix
+# y - class label vector
+# nmin - minimum number of observations in a node
+# minleaf - minimum number of observations in a leaf
+tree.grow <- function(x, y, nmin, minleaf){
+  
+}
+
+# x - input data matrix
+# tr - a tree object
+tree.classify <- function(x, tr){
+  
+}
+
+credit.dat <- read.csv('~/UU/MDM/credit.txt')
+
+classes <- (credit.dat[,6])
+
+a <- optimalsplit(credit.dat, classes, 4)
 
 
-optimalsplit(credit.dat[,4],credit.dat[,6])
+
+# income <- (credit.dat[,4])
+# input <- data.frame(income, classes)
+# input <- input[order(input[,1]),]
+# 
+# test <- subset(input, input[,1] > 52)
+# test2 <- subset(input, input[,1] < 52)
 
