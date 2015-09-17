ca8b43dd0e32460a8fd63f142894d18d2e82310a
Sebastiaan Jong <sebastiaan.jong@gmail.com>
2015-09-17 20:22:22 +0200

    a bunch of functions we are going to need

--- assignment1/code.R
+++ assignment1/code.R
@@ -13,29 +13,61 @@ splitpoints <- function(vect){
   return (splitpoints)
 }
 
-# x - input dataframe
-# y - classes vector
-# c - column index
-optimalsplit <- function(x, y, c){
-  potentialsplits <- splitpoints(x[,c])
+# Returns all possible splits (yay bruteforcing).
+# x - input data matrix.
+# y - class label vector.
+# nmin - minimum number of observations in a node.
+# minleaf - minimum number of observations in a leaf.
+allsplits <- function(x, y, nmin, minleaf){
   
-  splits <- mapply(function(pivot) split(x, y, c, pivot), potentialsplits)
-  
-  print(splits)
-
-  return(potentialsplits)
 }
 
-# split the dataset into two parts and return the impurity for this split
+# Given a list of splits, filter out the splits that do not adhere to the nmin and minleaf constraints.
+# splits - a list of splits.
+# nmin - minimum number of observations in a node.
+# minleaf - minimum number of observations in a leaf.
+filtersplits <- function(splits, nmin, minleaf){
+  eligible <- (apply(splits, 2, function(s) checksplit(s, nmin, minleaf)))
+  return (splits[,eligible])
+}
+
+# Filtersplits helper function.
+checksplit <- function(split, nmin, minleaf){
+  i <- split[[1]] # impurity is stored in the first position
+  size.left <- length(split[[2]]) # left part of the split
+  size.right <- length(split[[3]]) # right part of the split
+  
+  if (i == 0){ # its a leaf
+    return (size.left >= minleaf & size.right >= minleaf)
+  } else { # its an internal node
+    return (size.left >= nmin & size.right >= nmin)
+  }
+}
+
+# Returns a list of all possible splits on one column with their scores.
+# x - input dataframe.
+# y - classes vector.
+# c - column index.
+columnsplits <- function(x, y, c){
+  potentialsplits <- splitpoints(x[,c])
+  splits <- mapply(function(pivot) split(x, y, c, pivot), potentialsplits)
+  return(splits)
+}
+
+# Split the dataset into two parts and return the impurity for this split.
+# x - input dataframe.
+# y - classes vector.
+# c - column index.
+# pivot - value to split on.
 split <- function(x, y, c, pivot){
   ordering <- order(x[,c])
   x.ordered <- x[ordering,]
   y.ordered <- y[ordering]
   
-  x.left <- subset(x.ordered, x.ordered[,c] < pivot)
-  x.right <- subset(x.ordered, x.ordered[,c] > pivot)
+  x.left <- which(x.ordered[,c] < pivot)
+  x.right <- which(x.ordered[,c] > pivot)
   
-  offset <- nrow(x.left)
+  offset <- length(x.left)
   
   y.left <- y.ordered[1:offset]
   y.right <- y.ordered[offset+1:(length(y)-offset)]
@@ -48,32 +80,36 @@ split <- function(x, y, c, pivot){
   return(list(score, x.left, x.right))
 }
 
-# x - input data matrix
-# y - class label vector
-# nmin - minimum number of observations in a node
-# minleaf - minimum number of observations in a leaf
+# Grow a tree.
+# x - input data matrix.
+# y - class label vector.
+# nmin - minimum number of observations in a node.
+# minleaf - minimum number of observations in a leaf.
 tree.grow <- function(x, y, nmin, minleaf){
   
 }
 
+# Predict the classes of x.
 # x - input data matrix
 # tr - a tree object
 tree.classify <- function(x, tr){
   
 }
 
-credit.dat <- read.csv('~/UU/MDM/credit.txt')
+# tr - a tree object
+tree.simplify <- function(tr){
+  
+}
+
+credit.dat <- read.csv('~/UU/MDM/datamining/assignment1/data/credit.txt')
 
 classes <- (credit.dat[,6])
 
-a <- optimalsplit(credit.dat, classes, 4)
+a <- columnsplits(credit.dat, classes, 4)
+s <- filtersplits(a,2,1)
+
+print(a)
+print(s)
 
 
-
-# income <- (credit.dat[,4])
-# input <- data.frame(income, classes)
-# input <- input[order(input[,1]),]
-# 
-# test <- subset(input, input[,1] > 52)
-# test2 <- subset(input, input[,1] < 52)
 
